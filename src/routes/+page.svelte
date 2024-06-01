31a66e349b70a0fad3741b1677c38e537b603d5e
Richard <richard@stromer.org>
2024-06-01 17:20:22 +0200

    add new home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Bilderbuch</h1>
+
+  <ul>
+    <li>
+      <a href="/importer">Import photos</a>
+    </li>
+    <li>
+      <a href="/settings">Settings</a>
+    </li>
+  </ul>
+</main>
