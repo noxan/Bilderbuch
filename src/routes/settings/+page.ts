0bbe2d2558ce78afcb31cc49bc73fe26efaad31f
Richard <richard@stromer.org>
2024-06-22 15:57:54 +0200

    check if avatar file exists, example code

--- src/routes/settings/+page.ts
+++ src/routes/settings/+page.ts
@@ -1,7 +1,14 @@
 import type { PageLoad } from "./$types";
 
-export const load: PageLoad = ({ params }) => {
+import { BaseDirectory, exists } from '@tauri-apps/plugin-fs';
+
+
+export const load: PageLoad = async ({ params }) => {
+  // Check if the `$APPDATA/avatar.png` file exists
+  const exists = await exists('avatar.png', { baseDir: BaseDirectory.AppData });
+
   return {
+    exists,
     post: {
       title: `Title for post goes here`,
       content: `Content for post goes here`,
