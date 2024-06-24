4837009375a32ac542b5333eae6cb0d64a64818e
Richard <richard@stromer.org>
2024-06-24 10:43:05 +0200

    setup and run prettier with svelte config

--- src/routes/settings/+page.ts
+++ src/routes/settings/+page.ts
@@ -1,33 +1,46 @@
-import { BaseDirectory, exists, mkdir, readFile, writeFile } from '@tauri-apps/plugin-fs';
+import {
+  BaseDirectory,
+  exists,
+  mkdir,
+  readFile,
+  writeFile,
+} from "@tauri-apps/plugin-fs";
 import type { PageLoad } from "./$types";
 
 async function writeSettings(settings: object) {
   let encoder = new TextEncoder();
   let data = encoder.encode(JSON.stringify(settings));
-  return writeFile('settings.json', data, { baseDir: BaseDirectory.AppConfig, create: true });
+  return writeFile("settings.json", data, {
+    baseDir: BaseDirectory.AppConfig,
+    create: true,
+  });
 }
 
 async function readSettings() {
-  const raw = await readFile('settings.json', { baseDir: BaseDirectory.AppConfig });
+  const raw = await readFile("settings.json", {
+    baseDir: BaseDirectory.AppConfig,
+  });
   const decoder = new TextDecoder();
   const data = decoder.decode(raw);
   const userSettings = JSON.parse(data);
   return {
     ...defaultSettings,
     ...userSettings,
-  }
+  };
 }
 
 const defaultSettings = {
   success: true,
-}
+};
 
 export const load: PageLoad = async ({ params }) => {
-  const fileExists = await exists('settings.json', { baseDir: BaseDirectory.AppConfig });
+  const fileExists = await exists("settings.json", {
+    baseDir: BaseDirectory.AppConfig,
+  });
   if (!fileExists) {
-    console.log('Create settings directory')
-    await mkdir('', {baseDir: BaseDirectory.AppConfig, recursive: true })
-    console.log('Creating settings.json');
+    console.log("Create settings directory");
+    await mkdir("", { baseDir: BaseDirectory.AppConfig, recursive: true });
+    console.log("Creating settings.json");
     await writeSettings({ success: true });
   }
 
