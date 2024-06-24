b84142b35f1ee249a62f7ee83ee0691c4e3557c4
Richard <richard@stromer.org>
2024-06-24 09:57:33 +0200

    try to create a settings file

--- src/routes/settings/+page.ts
+++ src/routes/settings/+page.ts
@@ -1,10 +1,20 @@
+import { appConfigDir, resolve } from "@tauri-apps/api/path";
 import type { PageLoad } from "./$types";
 
-import { BaseDirectory, exists } from '@tauri-apps/plugin-fs';
+import { BaseDirectory, exists, mkdir, writeFile } from '@tauri-apps/plugin-fs';
 
 
 export const load: PageLoad = async ({ params }) => {
-  const fileExists = await exists('avatar.png', { baseDir: BaseDirectory.AppConfig });
+  const fileExists = await exists('settings.json', { baseDir: BaseDirectory.AppConfig });
+  if (!fileExists) {
+    console.log('Create settings directory')
+    const settingsDirectory = await appConfigDir();
+    await mkdir(settingsDirectory, { recursive: true })
+    console.log('Creating settings.json');
+    let encoder = new TextEncoder();
+    let data = encoder.encode(JSON.stringify({ success: true }));
+    await writeFile('settings.json', data, { baseDir: BaseDirectory.AppConfig, create: true });
+  }
 
   return {
     fileExists,
